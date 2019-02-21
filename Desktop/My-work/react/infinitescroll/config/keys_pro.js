70f5527734f96ac9ca3de6832c17f394da28078c
iAli <iali@alis-MacBook-Air.local>
2019-02-22 00:20:59 +0200

    infinity-v1.1

---
+++ Desktop/My-work/react/infinitescroll/config/keys_pro.js
@@ -0,0 +1,5 @@
+module.exports = {
+  APPLACATION_ID: process.env.APPLACATION_ID,
+  SECRET_KEY: process.env.SECRET_KEY,
+  CALLBACK_URL: process.env.CALLBACK_URL || "http://localhost:3000"
+};
--- Desktop/My-work/react/infinitescroll/config/server.js
+++ Desktop/My-work/react/infinitescroll/config/server.js
@@ -1,10 +1,6 @@
 // KEYS
-module.exports = {
-  APPLACATION_ID:
-    process.env.APPLACATION_ID ||
-    "13367fc05b3e2e73eb65eb2aa913f4355d54be56ce78cc31eede9465e71b989f",
-  SECRET_KEY:
-    process.env.SECRET_KEY ||
-    "1f287dc202f2e3f50bc2e35cf2f061d503e16780b87053d1f037b6a5af8d6f3c",
-  CALLBACK_URL: process.env.CALLBACK_URL || "http://localhost:3000"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
